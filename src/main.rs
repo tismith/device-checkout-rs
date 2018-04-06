a6677bfcee309e80b9c5a98e5fe3b9cc578707af
Toby Smith <toby@tismith.id.au>
2018-04-06 20:49:27 +1000

    Add some more custom logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 // `error_chain!` can recurse deeply
 #![recursion_limit = "1024"]
 
+//standard includes
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate log;
+//get logging from env, RUST_LOG=debug cargo run etc
+extern crate env_logger;
+
+//application specific includes
+extern crate websocket;
 
 pub mod errors {
     // Create the Error, ErrorKind, ResultExt, and Result types
@@ -18,6 +26,8 @@ quick_main!(run);
 // `errors` module. It is a typedef of the standard `Result` type
 // for which the error type is always our own `Error`.
 fn run() -> Result<()> {
+    env_logger::init();
+    trace!("Entry to top level run()");
     //DO STUFF
 
     Ok(())
