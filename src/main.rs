31cef66aa9fdec8ac1b39c7ce761f343b31c6175
Toby Smith <toby@tismith.id.au>
2018-04-26 15:01:14 +1000

    rocket JSON example integrated

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,59 @@ extern crate stderrlog;
 #[macro_use]
 extern crate clap;
 extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 mod utils;
 
 #[get("/")]
 fn index() -> &'static str {
-    trace!("default route called");
+    trace!("index()");
     "Hello, world!"
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ReservationStatus {
+    Available,
+    Reserved
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Device {
+    device_name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    device_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    device_owner: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    comments: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    reservation_status: Option<ReservationStatus>
+}
+
+#[get("/api/device/<name>")]
+fn api_get_device(name: String) -> rocket_contrib::Json<Device> {
+    trace!("api_get_device()");
+    rocket_contrib::Json(Device {
+        device_name:name,
+        device_url: None,
+        device_owner: None,
+        comments: None,
+        reservation_status: Some(ReservationStatus::Available),
+    })
 }
 
 fn run(_config: &utils::types::Settings) -> Result<(), failure::Error> {
     trace!("Entry to top level run()");
 
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, api_get_device]).launch();
 
     Ok(())
 }
