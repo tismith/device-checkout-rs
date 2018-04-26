00de4a89bfc43c0af4b293c6ae97268bc77869c7
Toby Smith <toby@tismith.id.au>
2018-04-26 16:22:30 +1000

    Remove bogus tests that assume the bin exits

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,12 +1,6 @@
 extern crate assert_cli;
 
 #[test]
-fn test_basic() {
-    //test that it runs
-    assert_cli::Assert::main_binary().unwrap();
-}
-
-#[test]
 fn test_help() {
     //test that help works contains a USAGE string
     assert_cli::Assert::main_binary()
@@ -23,11 +17,6 @@ fn test_verbosity_limit() {
         .with_args(&["-vvvvv"])
         .fails()
         .unwrap();
-
-    //test that an acceptable number succeeds
-    assert_cli::Assert::main_binary()
-        .with_args(&["-vv"])
-        .unwrap();
 }
 
 #[test]
@@ -36,15 +25,5 @@ fn test_timestamp_parsing() {
     assert_cli::Assert::main_binary()
         .with_args(&["-t", "bogus"])
         .fails()
-        .unwrap();
-
-    //and that a real timestamp succeeds
-    assert_cli::Assert::main_binary()
-        .with_args(&["-t", "sec"])
-        .unwrap();
-
-    //and that a long argument timestamp succeeds
-    assert_cli::Assert::main_binary()
-        .with_args(&["--timestamp", "ms"])
         .unwrap();
 }
