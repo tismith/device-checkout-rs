92ab993e919bf11b9e61e712544e2c468bbf1f43
Toby Smith <toby@tismith.id.au>
2018-04-28 16:48:03 +1000

    Add more constraints to the database

--- migrations/2018-04-26-062406_create_devices/up.sql
+++ migrations/2018-04-26-062406_create_devices/up.sql
@@ -3,9 +3,9 @@ create table devices (
 	id integer primary key not null,
 	device_name text unique not null,
 	device_url text,
-	device_owner text,
+	device_owner text check((reservation_status = 'reserved' and device_owner is not null) or (reservation_status = 'available')),
 	comments text,
-	reservation_status text check(reservation_status in ('available', 'reserved')) not null,
+	reservation_status text check(reservation_status in ('available', 'reserved')) not null default 'available',
 	created_at timestamp default current_timestamp,
 	updated_at timestamp default current_timestamp
 );
