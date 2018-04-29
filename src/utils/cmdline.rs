732a5158de187bef12b30280dce4a0a0d3aacf7f
Toby Smith <toby@tismith.id.au>
2018-04-30 07:24:32 +1000

    Parse port number from cmdline

--- src/utils/cmdline.rs
+++ src/utils/cmdline.rs
@@ -55,12 +55,29 @@ pub fn parse_cmdline() -> types::Settings {
             info: None,
         }.exit(),
     };
-    //TODO parse port number
+
+    let parsed_port = match matches.value_of("port") {
+        Some(raw_port) => raw_port.parse(),
+        _ => clap::Error {
+            message: "invalid value for 'port'".into(),
+            kind: clap::ErrorKind::InvalidValue,
+            info: None,
+        }.exit(),
+    };
+    let port = match parsed_port {
+        Ok(port) => port,
+        _ => clap::Error {
+            message: "invalid value for 'port'".into(),
+            kind: clap::ErrorKind::InvalidValue,
+            info: None,
+        }.exit(),
+    };
 
     types::Settings {
         verbosity,
         quiet,
         timestamp,
+        port,
         ..Default::default()
     }
 }
