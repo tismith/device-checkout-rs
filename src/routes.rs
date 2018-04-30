963b3bc6bf105e4484a056993a95697ac3a15eee
Toby Smith <toby@tismith.id.au>
2018-04-30 14:41:12 +1000

    Add some more templating logic

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,9 @@
-use std;
 use database;
 use failure;
 use models;
 use rocket;
 use rocket_contrib;
+use std;
 use utils;
 
 #[get("/")]
@@ -36,7 +36,32 @@ pub fn get_devices(
     config: rocket::State<utils::types::Settings>,
 ) -> Result<rocket_contrib::Template, failure::Error> {
     trace!("get_devices()");
-    let devices = database::get_devices(&*config)?;
+
+    #[derive(Serialize)]
+    struct GetDevicesContext {
+        device: models::Device,
+        button_string: String,
+        button_class: String,
+    }
+
+    let devices: Vec<_> = database::get_devices(&*config)?
+        .into_iter()
+        .map(|d| {
+            let button_string = match d.reservation_status {
+                models::ReservationStatus::Reserved => "RETURN".to_string(),
+                _ => "CLAIM".to_string(),
+            };
+            let button_class = match d.reservation_status {
+                models::ReservationStatus::Reserved => "btn-danger".to_string(),
+                _ => "btn-primary".to_string(),
+            };
+            GetDevicesContext {
+                device: d,
+                button_string,
+                button_class,
+            }
+        })
+        .collect();
     let mut context = std::collections::HashMap::new();
     context.insert("devices", devices);
     Ok(rocket_contrib::Template::render("devices", &context))
