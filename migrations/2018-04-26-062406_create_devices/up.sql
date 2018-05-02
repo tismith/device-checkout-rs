51389eecdbf9bafc4f42e5027821df8aca2a4d94
Toby Smith <toby@tismith.id.au>
2018-05-02 15:22:02 +1000

    Minor tidy up of db schema

--- migrations/2018-04-26-062406_create_devices/up.sql
+++ migrations/2018-04-26-062406_create_devices/up.sql
@@ -5,20 +5,20 @@ create table devices (
 	device_url text,
 	device_owner text,
 	comments text,
-	reservation_status text
+	reservation_status text not null default 'available',
 	created_at timestamp default current_timestamp not null,
-	updated_at timestamp default current_timestamp not null
+	updated_at timestamp default current_timestamp not null,
 	--device_name not empty
 	check (device_name <> '')
 	--reservation_status is an enum
-	check(reservation_status in ('available', 'reserved')) not null default 'available',
+	check(reservation_status in ('available', 'reserved'))
 	--if we're reserved, then we need a not empty device_owner
 	check (reservation_status <> "reserved" or (device_owner is not null and device_owner <> ''))
 );
 
 create trigger devices after update on devices
 begin
-	update devices set updated_at = datetime('now') where id = NEW.id;
+	update devices set updated_at = current_timestamp where id = NEW.id;
 end;
 
 insert into devices (device_name, device_url) values ("unit1", "http://unit1");
