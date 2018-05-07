bdada2bfe7ffead73299c2275be6cd93b7ea5430
Toby Smith <toby@tismith.id.au>
2018-05-07 18:37:14 +1000

    Move to a lib/bin split for testing

---
+++ src/bin/device-checkout.rs
@@ -0,0 +1,49 @@
+extern crate device_checkout_lib;
+use device_checkout_lib::*;
+
+#[macro_use]
+extern crate log;
+
+fn run(config: utils::types::Settings) -> Result<(), failure::Error> {
+    trace!("run()");
+
+    database::run_migrations(&config)?;
+
+    let rocket_config = rocket::config::Config::build(rocket::config::Environment::Production)
+        .port(config.port)
+        .finalize()?;
+
+    rocket::custom(rocket_config, true)
+        .manage(database_pool::init_pool(&config))
+        .manage(config)
+        .attach(rocket_contrib::Template::fairing())
+        .mount(
+            "/",
+            routes::html_routes()
+        )
+        .mount(
+            "/api/",
+            routes::api_routes()
+        )
+        .launch();
+
+    Ok(())
+}
+
+fn main() {
+    let mut config = utils::cmdline::parse_cmdline();
+    config.module_path = Some(module_path!().into());
+    utils::logging::configure_logger(&config);
+
+    if let Err(ref e) = run(config) {
+        use failure::Fail;
+        let mut fail: &Fail = e.cause();
+        error!("{}", fail);
+
+        while let Some(cause) = fail.cause() {
+            error!("caused by: {}", cause);
+            fail = cause;
+        }
+        std::process::exit(1);
+    }
+}
