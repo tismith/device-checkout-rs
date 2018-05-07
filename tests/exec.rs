7738d688abc4437eb88166d865fd5e50e39930a2
Toby Smith <toby@tismith.id.au>
2018-05-08 09:43:54 +1000

    Try out codecov for travis

--- tests/exec.rs
+++ tests/exec.rs
@@ -1,9 +1,17 @@
 extern crate assert_cli;
 
+//kcov doesn't play nice with assert_cli() see
+//https://github.com/assert-rs/assert_cli/issues/101
+use std::env;
+fn get_cwd() -> String {
+    env::current_dir().unwrap().to_str().unwrap().to_string()
+}
+
 #[test]
 fn test_help() {
     //test that help works contains a USAGE string
-    assert_cli::Assert::main_binary()
+    let bin: &str = &format!("{}/target/debug/device-checkout", get_cwd());
+    assert_cli::Assert::command(&[bin])
         .with_args(&["-h"])
         .stdout()
         .contains("USAGE")
@@ -13,7 +21,8 @@ fn test_help() {
 #[test]
 fn test_verbosity_limit() {
     //test that we error out with too many verbosity flags
-    assert_cli::Assert::main_binary()
+    let bin: &str = &format!("{}/target/debug/device-checkout", get_cwd());
+    assert_cli::Assert::command(&[bin])
         .with_args(&["-vvvvv"])
         .fails()
         .unwrap();
@@ -22,7 +31,8 @@ fn test_verbosity_limit() {
 #[test]
 fn test_timestamp_parsing() {
     //test that a bogus timestamp errors out
-    assert_cli::Assert::main_binary()
+    let bin: &str = &format!("{}/target/debug/device-checkout", get_cwd());
+    assert_cli::Assert::command(&[bin])
         .with_args(&["-t", "bogus"])
         .fails()
         .unwrap();
