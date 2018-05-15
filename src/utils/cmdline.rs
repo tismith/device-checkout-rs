647ae44b36dda8b5b0813e81aaa3e51df6003660
Toby Smith <toby@tismith.id.au>
2018-05-15 19:26:31 +1000

    test: add extra cmdline tests

--- src/utils/cmdline.rs
+++ src/utils/cmdline.rs
@@ -2,18 +2,21 @@ use clap;
 use utils::types;
 
 pub fn parse_cmdline() -> types::Settings {
-    let matches = arg_matcher().get_matches();
-    parse_matcher(&matches)
+    let matches = matcher().get_matches();
+    match parse(&matches) {
+        Ok(s) => s,
+        Err(e) => e.exit(),
+    }
 }
 
-fn parse_matcher(matches: &clap::ArgMatches) -> types::Settings {
+fn parse(matches: &clap::ArgMatches) -> Result<types::Settings, clap::Error> {
     let verbosity = matches.occurrences_of("verbosity") as usize;
     if verbosity > 4 {
-        clap::Error {
+        Err(clap::Error {
             message: "invalid number of 'v' flags".into(),
             kind: clap::ErrorKind::InvalidValue,
             info: None,
-        }.exit()
+        })?
     }
     let quiet = matches.is_present("quiet");
     let timestamp = match matches.value_of("timestamp") {
@@ -21,33 +24,31 @@ fn parse_matcher(matches: &clap::ArgMatches) -> types::Settings {
         Some("ms") => types::Timestamp::Microsecond,
         Some("sec") => types::Timestamp::Second,
         Some("none") | None => types::Timestamp::Off,
-        Some(_) => clap::Error {
+        Some(_) => Err(clap::Error {
             message: "invalid value for 'timestamp'".into(),
             kind: clap::ErrorKind::InvalidValue,
             info: None,
-        }.exit(),
+        })?,
     };
 
-    let port = value_t!(matches.value_of("port"), u16).unwrap_or_else(|e| e.exit());
-    let database = matches.value_of("database").unwrap_or_else(|| {
-        clap::Error {
-            message: "invalid value for 'database'".into(),
-            kind: clap::ErrorKind::InvalidValue,
-            info: None,
-        }.exit()
-    });
+    let port = value_t!(matches.value_of("port"), u16)?;
+    let database = matches.value_of("database").ok_or_else(|| clap::Error {
+        message: "invalid value for 'database'".into(),
+        kind: clap::ErrorKind::InvalidValue,
+        info: None,
+    })?;
 
-    types::Settings {
+    Ok(types::Settings {
         verbosity,
         quiet,
         timestamp,
         port,
         database_url: database.to_string(),
         ..Default::default()
-    }
+    })
 }
 
-fn arg_matcher<'a, 'b>() -> clap::App<'a, 'b> {
+fn matcher<'a, 'b>() -> clap::App<'a, 'b> {
     clap::App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
@@ -95,21 +96,48 @@ mod tests {
 
     #[test]
     fn test_port() {
-        let m = arg_matcher()
+        let m = matcher()
             .get_matches_from_safe(vec!["", "--port", "1234"])
             .unwrap();
-        let s = parse_matcher(&m);
+        let s = parse(&m).unwrap();
 
         assert_eq!(s.port, 1234u16);
     }
 
     #[test]
     fn test_database() {
-        let m = arg_matcher()
+        let m = matcher()
             .get_matches_from_safe(vec!["", "--database", "somefile.txt"])
             .unwrap();
-        let s = parse_matcher(&m);
+        let s = parse(&m).unwrap();
 
         assert_eq!(s.database_url, "somefile.txt".to_string());
+    }
+
+    #[test]
+    fn test_too_much_verbosity() {
+        let m = matcher().get_matches_from_safe(vec!["", "-vvvvv"]).unwrap();
+        assert!(parse(&m).is_err());
+    }
+
+    #[test]
+    fn test_just_enough_verbosity() {
+        let m = matcher().get_matches_from_safe(vec!["", "-vvv"]).unwrap();
+        let s = parse(&m).unwrap();
+
+        assert_eq!(s.verbosity, 3);
+    }
+
+    #[test]
+    fn test_timestamps() {
+        let m = matcher()
+            .get_matches_from_safe(vec!["", "-t", "sec"])
+            .unwrap();
+        let s = parse(&m).unwrap();
+
+        match s.timestamp {
+            types::Timestamp::Second => (),
+            _ => panic!("unexpected parse"),
+        }
     }
 }
