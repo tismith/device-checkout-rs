6b9be7a16f66dcbd720655edd358bd5f8d805712
Toby Smith <toby@tismith.id.au>
2018-06-18 12:13:56 +1000

    feat: add more context on errors

--- src/bin/device-checkout.rs
+++ src/bin/device-checkout.rs
@@ -1,11 +1,12 @@
 extern crate device_checkout_lib;
 use device_checkout_lib::*;
+use failure::ResultExt;
 
 fn main() -> Result<(), exitfailure::ExitFailure> {
     let mut config = utils::cmdline::parse_cmdline();
     config.module_path = Some(module_path!().into());
     utils::logging::configure_logger(&config);
-    database::run_migrations(&config)?;
-    routes::rocket(config)?.launch();
+    database::run_migrations(&config).context("Failed to migrate database")?;
+    routes::rocket(config).context("Failed to launch Rocket http engine")?.launch();
     Ok(())
 }
